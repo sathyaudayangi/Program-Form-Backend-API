f4bec5a3a8a6b5ad4bf7a396fad188a7481b3b15
Sathya Karunakalage <82775587+sathyaudayangi@users.noreply.github.com>
2024-05-14 23:25:57 +0530

    Add program controller

--- Controllers/ProgramController.cs
+++ Controllers/ProgramController.cs
@@ -1,6 +1,55 @@
-﻿namespace Program_Form_Backend_API.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Program_Form_Backend_API.Models;
+using Microsoft.Azure.Cosmos;
+using Program_Form_Backend_API.Services;
+
+namespace Program_Form_Backend_API.Controllers
 {
-    public class ProgramController
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProgramsController : ControllerBase
     {
+        private readonly DBService _dbService;
+
+        public ProgramsController()
+        {
+            _dbService = DBService.Instance;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProgram([FromBody] CompanyProgram program)
+        {
+            try
+            {
+                await _dbService.ProgramContainer.CreateItemAsync(program, new PartitionKey(program.ProgramId));
+
+                return CreatedAtAction(nameof(GetProgramById), new { id = program.Id }, program);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProgramById(string id)
+        {
+            try
+            {
+                var programResponse = await _dbService.ProgramContainer.ReadItemAsync<CompanyProgram>(id, new PartitionKey(id));
+                return Ok(programResponse.Resource);
+            }
+            catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
     }
 }
